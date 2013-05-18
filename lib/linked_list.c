8866b26b3cfe473e2cf7b061a4aea1218359a7a6
Ray Imber <ray.imber@galavantier.com>
2013-05-17 23:43:51 -0700

    Linked List Thread safety.

--- lib/linked_list.c
+++ lib/linked_list.c
@@ -4,12 +4,13 @@
 * Ray Imber
 * Implements a simple linked list data structure. Singly Linked for simplicity.
 * NOTE: The first node has NULL data by convention.
-* @TODO: This library is not thread safe. Add some locks to make it thread safe.
+* The thread safe version of each function is suffixed with ts, i.e. linkedList_addNode_ts() 
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
 
 typedef void (*print_node_data_function)(void*);
 typedef void* (*iterateFunction)(void*);
@@ -25,6 +26,7 @@ typedef struct
   linkedListNode* head;
   linkedListNode* tail; // I'm cheating, but screw it :-P
   linkedListNode* curNode; // Used to implement an iterator.
+  pthread_mutex_t lock;
 } linkedListStruct;
 
 linkedListStruct* linkedList_newList()
@@ -44,9 +46,26 @@ linkedListStruct* linkedList_newList()
   return newList;
 }
 
+linkedListStruct* linkedList_newList_ts()
+{
+  linkedListStruct* newList;
+  newList = linkedList_newList();
+  pthread_mutex_init( &(newList->lock), NULL );
+  return newList;
+}
+
 int linkedList_isEmptyList(linkedListStruct* cur_list)
 {
   return ( cur_list->head == cur_list->tail );
+}
+
+int linkedList_isEmptyList_ts(linkedListStruct* cur_list)
+{
+  pthread_mutex_lock(&(cur_list->lock));
+  int retVal = linkedList_isEmptyList(cur_list);
+  pthread_mutex_unlock(&(cur_list->lock));
+  
+  return retVal;
 }
 
 linkedListNode* linkedList_addNode(linkedListStruct* cur_list, void* newData)
@@ -69,9 +88,24 @@ linkedListNode* linkedList_addNode(linkedListStruct* cur_list, void* newData)
   return newNode;
 }
 
+linkedListNode* linkedList_addNode_ts(linkedListStruct* cur_list, void* newData)
+{
+  pthread_mutex_lock(&(cur_list->lock));
+  linkedListNode* retVal = linkedList_addNode(cur_list, newData);
+  pthread_mutex_unlock(&(cur_list->lock));
+  return retVal;
+}
+
 void linkedList_reset_iterator(linkedListStruct* cur_list)
 {
   cur_list->curNode = cur_list->head;
+}
+
+void linkedList_reset_iterator_ts(linkedListStruct* cur_list)
+{
+  pthread_mutex_lock(&(cur_list->lock));
+  linkedList_reset_iterator(cur_list);
+  pthread_mutex_unlock(&(cur_list->lock));
 }
 
 linkedListNode* linkedList_iterate(linkedListStruct* cur_list)
@@ -86,6 +120,14 @@ linkedListNode* linkedList_iterate(linkedListStruct* cur_list)
     cur_list->curNode = cur_list->head;
     return NULL;
   }
+}
+
+linkedListNode* linkedList_iterate_ts(linkedListStruct* cur_list)
+{
+  pthread_mutex_lock(&(cur_list->lock));
+  linkedListNode* retVal = linkedList_iterate(cur_list);
+  pthread_mutex_unlock(&(cur_list->lock));
+  return retVal;
 }
 
 void* linkedList_foreach(linkedListStruct* cur_list)
@@ -104,6 +146,14 @@ void* linkedList_foreach(linkedListStruct* cur_list)
     }
   }
   return NULL;
+}
+
+void* linkedList_foreach_ts(linkedListStruct* cur_list)
+{
+  pthread_mutex_lock(&(cur_list->lock));
+  void* retVal = linkedList_foreach(cur_list);
+  pthread_mutex_unlock(&(cur_list->lock));
+  return retVal;
 }
 
 void linkedList_removeNode(linkedListStruct* cur_list, linkedListNode* nodeToDelete)
@@ -130,6 +180,13 @@ void linkedList_removeNode(linkedListStruct* cur_list, linkedListNode* nodeToDel
     }
     prevNode = curNode;
   } while(curNode != NULL);
+}
+
+void linkedList_removeNode_ts(linkedListStruct* cur_list, linkedListNode* nodeToDelete)
+{
+  pthread_mutex_lock(&(cur_list->lock));
+  linkedList_removeNode(cur_list, nodeToDelete);
+  pthread_mutex_unlock(&(cur_list->lock)); 
 }
 
 void linkedList_printList(linkedListStruct* cur_list, print_node_data_function output)
@@ -186,6 +243,13 @@ void linkedList_findAndRemoveNode(linkedListStruct* cur_list, void* dataToFind)
   }
 }
 
+void linkedList_findAndRemoveNode_ts(linkedListStruct* cur_list, void* dataToFind)
+{
+  pthread_mutex_lock(&(cur_list->lock));
+  linkedList_findAndRemoveNode(cur_list, dataToFind);
+  pthread_mutex_unlock(&(cur_list->lock));
+}
+
 void linkedList_freeAllButFirst(linkedListStruct* cur_list)
 {
   linkedListNode* firstNode;
@@ -212,6 +276,41 @@ void linkedList_freeAllButFirst(linkedListStruct* cur_list)
   }
 }
 
+void linkedList_freeAllButFirst_ts(linkedListStruct* cur_list)
+{
+  pthread_mutex_lock(&(cur_list->lock));
+  linkedList_freeAllButFirst(cur_list);
+  pthread_mutex_lock(&(cur_list->lock));
+}
+
+void* linkedList_pop(linkedListStruct* cur_list)
+{
+  if(!linkedList_isEmptyList(cur_list))
+  {
+    // The true head is a placeholder, so get the real Head, which is the second Node.
+    linkedListNode* realHead = cur_list->head->nextNode;
+    void* retVal = realHead->data;
+
+    // Make sure the iterator doesn't break;
+    if(cur_list->curNode == realHead)
+    {
+      linkedList_iterate(cur_list);
+    }
+
+    linkedList_removeNode(cur_list, realHead);
+    return retVal;
+  }
+  return NULL;
+}
+
+void* linkedList_pop_ts(linkedListStruct* cur_list)
+{
+  pthread_mutex_lock(&(cur_list->lock));
+  void* retVal = linkedList_pop(cur_list);
+  pthread_mutex_lock(&(cur_list->lock));
+  return retVal;
+}
+
 void linkedList_free(linkedListStruct* cur_list)
 {
   cur_list->curNode = cur_list->head; // Reset the iterator to the head
@@ -221,7 +320,22 @@ void linkedList_free(linkedListStruct* cur_list)
     curNode = linkedList_iterate(cur_list);
     free(curNode);
   } while(curNode != NULL);
+
+  free(cur_list);
+}
+
+void linkedList_free_ts(linkedListStruct* cur_list)
+{
+  cur_list->curNode = cur_list->head; // Reset the iterator to the head
+  linkedListNode* curNode;
+  do
+  {
+    curNode = linkedList_iterate(cur_list);
+    free(curNode);
+  } while(curNode != NULL);
   
+  pthread_mutex_destroy(&(cur_list->lock));
+
   free(cur_list);
 }
 
